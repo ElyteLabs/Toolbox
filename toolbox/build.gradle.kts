b85070bfbb2834d01a03a0cafba8dfaa06fca032
ItzFaisal <thisizfaisal@outlook.com>
2024-01-13 00:06:33 +0530

    initial commit

--- toolbox/build.gradle.kts
+++ toolbox/build.gradle.kts
@@ -52,7 +52,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.elytelabs.toolbox"
             artifactId = "toolbox"
-            version = "1.0.0"
+            version = "1.1.0"
             afterEvaluate {
                 from(components["release"])
             }
